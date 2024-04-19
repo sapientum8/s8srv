9158f542c49f2bf68ae41eb01af9aa3aec5c120b
sapientum8 <sapientum08@gmail.com>
2024-04-19 18:27:48 -0400

    gradle changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,9 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
 }
 
-group = "com.serverless"
-version = "dev"
-
-description = """hello"""
+description = """s8srv project"""
+group = "com.sapientum8"
+version = "0.1"
 
 tasks {
     javadoc {
@@ -31,44 +30,49 @@ repositories {
 
 // If requiring AWS JDK, uncomment the dependencyManagement to use the bill of materials
 //   https://aws.amazon.com/blogs/developer/managing-dependencies-with-aws-sdk-for-java-bill-of-materials-module-bom/
-//dependencyManagement {
-//    imports {
-//        mavenBom 'com.amazonaws:aws-java-sdk-bom:1.11.688'
-//    }
-//}
+
+dependencyManagement {
+    imports {
+        mavenBom("software.amazon.awssdk:dynamodb:2.25.31")
+        mavenBom("software.amazon.awssdk:s3:2.25.31")
+    }
+}
 
 dependencies {
-    api("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.23")
+    api("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.0.0-Beta5")
     implementation("com.amazonaws:aws-lambda-java-core:1.2.3")
     implementation("com.amazonaws:aws-lambda-java-log4j2:1.6.0")
-    implementation("org.apache.logging.log4j:log4j-api:2.23.1")
-    implementation("org.apache.logging.log4j:log4j-core:2.23.1")
+    implementation("software.amazon.awssdk:dynamodb:2.25.31")
+    implementation("org.apache.logging.log4j:log4j-api:3.0.0-beta1")
+    implementation("org.apache.logging.log4j:log4j-core:3.0.0-beta1")
     implementation("com.fasterxml.jackson.core:jackson-core:2.17.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.17.0")
 }
 
-task<Exec>("deploy") {
-    dependsOn("shadowJar")
-    commandLine("cmd", "/c", "serverless", "deploy")
-}
-
-task<Exec>("deploy-hello") {
-    dependsOn("shadowJar")
-    commandLine("cmd", "/c", "serverless", "deploy", "function", "-f", "hello")
-}
-
-task<Exec>("invoke-hello") {
-    dependsOn("shadowJar")
-    commandLine("cmd", "/c", "serverless", "invoke", "-f", "hello", "--log")
-}
-
 tasks {
+    register<Exec>("deploy") {
+        group = "serverless"
+        description = "Deploy the serverless application"
+        dependsOn("shadowJar")
+        commandLine("cmd", "/c", "serverless", "deploy")
+    }
+    register<Exec>("deploy-hello") {
+        group = "serverless"
+        description = "Deploy the serverless function hello"
+        dependsOn("shadowJar")
+        commandLine("cmd", "/c", "serverless", "deploy", "function", "-f", "hello")
+    }
+    register<Exec>("invoke-hello") {
+        group = "serverless"
+        description = "Invoke the serverless function hello"
+        dependsOn("shadowJar")
+        commandLine("cmd", "/c", "serverless", "invoke", "-f", "hello", "--log")
+    }
     named<ShadowJar>("shadowJar") {
         transform(com.github.jengelman.gradle.plugins.shadow.transformers.Log4j2PluginsCacheFileTransformer::class.java)
     }
-}
-
-tasks.build {
-    finalizedBy("shadowJar")
+    build {
+        finalizedBy("shadowJar")
+    }
 }
--- src/main/kotlin/com/serverless/Handler.kt
+++ src/main/kotlin/com/serverless/Handler.kt
@@ -3,19 +3,55 @@ package com.serverless
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import org.apache.logging.log4j.LogManager
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest
+import kotlin.system.exitProcess
 
-class Handler:RequestHandler<Map<String, Any>, ApiGatewayResponse> {
-  override fun handleRequest(input:Map<String, Any>, context:Context):ApiGatewayResponse {
-    LOG.info("received: " + input.keys.toString())
+class Handler : RequestHandler<Map<String, Any>, ApiGatewayResponse> {
+    override fun handleRequest(input: Map<String, Any>, context: Context): ApiGatewayResponse {
+        LOG.info("received: " + input.keys.toString())
 
-    return ApiGatewayResponse.build {
-      statusCode = 200
-      objectBody = HelloResponse("Go Serverless v2.x! Your Kotlin function executed successfully!", input)
-      headers = mapOf("X-Powered-By" to "AWS Lambda & serverless")
+        val client: DynamoDbClient = DynamoDbClient.builder()
+            .region(Region.US_EAST_1)
+            .build()
+
+        getDynamoDBItem(client, "Books", "book", "book101");
+
+        client.close();
+
+        return ApiGatewayResponse.build {
+            statusCode = 200
+            objectBody = HelloResponse("Go Serverless v2.3! Your Kotlin function executed successfully!", input)
+            headers = mapOf("X-Powered-By" to "AWS Lambda & serverless")
+        }
     }
-  }
 
-  companion object {
-    private val LOG = LogManager.getLogger(Handler::class.java)
-  }
+    companion object {
+        private val LOG = LogManager.getLogger(Handler::class.java)
+    }
+
+    fun getDynamoDBItem(ddb: DynamoDbClient, tableName: String?, key: String, keyVal: String?) {
+        val keyToGet = HashMap<String, AttributeValue>()
+        keyToGet[key] = AttributeValue.builder().s(keyVal).build()
+        val request = GetItemRequest.builder().key(keyToGet).tableName(tableName).build()
+        try {
+            // If there is no matching item, GetItem does not return any data.
+            val returnedItem = ddb.getItem(request).item()
+            if (returnedItem.isEmpty())
+                System.out.format("No item found with the key %s!\n", key)
+            else {
+                val keys: Set<String> = returnedItem.keys
+                println("Amazon DynamoDB table attributes: \n")
+                for (key1 in keys) {
+                    System.out.format("%s: %s\n", key1, returnedItem[key1].toString())
+                }
+            }
+        } catch (e: DynamoDbException) {
+            System.err.println(e.message)
+            exitProcess(1)
+        }
+    }
 }
